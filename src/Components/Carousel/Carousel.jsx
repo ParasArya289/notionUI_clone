9fa5f3f1e1f3bc70d959e0125168a395ac4b4973
Paras Arya <parasarya289@gmail.com>
2023-12-03 17:20:53 +0530

    feat: carousel

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,42 @@
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import "./Carousel.css";
+
+export const Carousel = ({
+  setIndex,
+  index,
+  imgArr,
+  controlls = true,
+  runEvery = 1000,
+}) => {
+  const handleNextImg = () => {
+    if (imgArr.length - 1 === index) {
+      setIndex(0);
+      return;
+    }
+    setIndex((prev) => prev + 1);
+  };
+  const handlePreviousImg = () => {
+    if (index === 0) {
+      setIndex(imgArr.length - 1);
+      return;
+    }
+    setIndex((prev) => prev - 1);
+  };
+  console.log(index);
+  return (
+    <div className="carousel">
+      <img src={imgArr[index]} />
+
+      <button className="previous_half" onClick={handlePreviousImg}>
+        <button className="previous_btn btn">
+          <IoIosArrowBack />
+        </button>
+      </button>
+      <button className="next_half" onClick={handleNextImg}>
+        <button className="next_btn btn">
+          <IoIosArrowForward />
+        </button>
+      </button>
+    </div>
+  );
+};
