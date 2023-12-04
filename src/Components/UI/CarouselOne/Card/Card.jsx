6b9bc0c92c9164a5919d2f80401605c47870ee7a
Paras Arya <parasarya289@gmail.com>
2023-12-04 15:47:29 +0530

    feat: card scroll into view

--- src/Components/UI/CarouselOne/Card/Card.jsx
+++ src/Components/UI/CarouselOne/Card/Card.jsx
@@ -1,18 +1,20 @@
 import { BsStars } from "react-icons/bs";
 import { IoArrowForward } from "react-icons/io5";
+import useScrollIntoView from "../../../../Hooks/useScrollIntoView";
 import "./Card.css";
 
 export const Card = ({ setIndex, index }) => {
+  const containerRef = useScrollIntoView(".card[data-active=true]", index);
   const handleAction = (index) => {
     setIndex(index);
   };
   return (
-    <div className="container">
+    <div ref={containerRef} className="container">
       <div
         className="card"
         onPointerEnter={() => handleAction(0)}
         onClick={() => handleAction(0)}
-        aria-active={index === 0}
+        data-active={index === 0}
         data-color="purple"
       >
         <div className="card__heading">
@@ -30,7 +32,7 @@ export const Card = ({ setIndex, index }) => {
         className="card"
         onPointerEnter={() => handleAction(1)}
         onClick={() => handleAction(1)}
-        aria-active={index === 1}
+        data-active={index === 1}
         data-color="red"
       >
         <div className="card__heading">
@@ -48,7 +50,7 @@ export const Card = ({ setIndex, index }) => {
         className="card"
         onPointerEnter={() => handleAction(2)}
         onClick={() => handleAction(2)}
-        aria-active={index === 2}
+        data-active={index === 2}
         data-color="blue"
       >
         <div className="card__heading">
@@ -66,7 +68,7 @@ export const Card = ({ setIndex, index }) => {
         className="card"
         onPointerEnter={() => handleAction(3)}
         onClick={() => handleAction(3)}
-        aria-active={index === 3}
+        data-active={index === 3}
         data-color="yellow"
       >
         <div className="card__heading">
